12189d446e378ef4dd62795038bf7b264f6887ce
trevorjtclarke <trevorjtclarke@gmail.com>
2021-04-10 16:31:23 -0700

    working on homepage

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="relative bg-gray-900">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <div class="nes-badge">
+            <span class="is-error">CRONCAT</span>
+          </div>
+        </div>
+        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0 flex">
+          <a href="https://github.com/Cron-Near/Cron-Spec" target="_blank" class="flex mr-10 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+            </svg>
+            <span>Docs</span>
+          </a>
+          <a href="" class="flex mr-10 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+            </svg>
+            <span>Tasks</span>
+          </a>
+          <a href="https://github.com/Cron-Near/croncat" target="_blank" class="flex mr-10 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+            <span>CLI</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,11 +3,87 @@
     Login
   </div> -->
   <div>
-    <img src="https://i.pinimg.com/originals/d2/b6/88/d2b688357b0c20cebde3745a3043108d.gif" alt="">
+    <Header />
+
+    <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <!-- <img src="https://i.pinimg.com/originals/d2/b6/88/d2b688357b0c20cebde3745a3043108d.gif" alt=""> -->
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-100">
+            "At minute 23 past every 2nd hour from 0 through 20."
+          </h2>
+          <p class="mt-2 text-center text-sm text-gray-600">
+            next at 2021-04-10 16:23:00
+          </p>
+        </div>
+        <form class="mt-8 space-y-6" action="#" method="POST">
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="email-address" class="sr-only">Email address</label>
+              <input id="email-address" name="email" type="email" autocomplete="email" placeholder="* * * * *" value="23 0-20/2 * * *" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+            </div>
+          </div>
+
+          <button type="button" class="nes-btn is-primary bg-pine-400">Create Task</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="py-12 bg-teal-900">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+          <h2 class="text-base text-sunset-500 font-semibold tracking-wide uppercase">What is croncat?</h2>
+          <p class="mt-2 text-3xl leading-4 font-extrabold tracking-tight text-gray-100 sm:text-4xl">
+            Decentralized Scheduling for Blockchain Transactions
+          </p>
+        </div>
+
+        <div class="mt-10">
+          <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 text-white">
+                  <!-- Heroicon name: outline/annotation -->
+                  <a class="nes-btn bg-sunset-500 hover:bg-sunset-500" href="#">
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                    </svg>
+                  </a>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-200">Mobile notifications</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+          </dl>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="bg-gray-700">
+      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+          <span class="block">What's running on croncat?</span>
+        </h2>
+        <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+          <div class="inline-flex rounded-md shadow">
+            <a class="nes-btn" href="#">See All Tasks</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- TODO: USE THIS! -->
+    <!-- <img src="https://i.pinimg.com/originals/d2/b6/88/d2b688357b0c20cebde3745a3043108d.gif" alt=""> -->
     <!-- <div class="nes-container is-rounded is-dark bg-gray-900">
       <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
     </div> -->
-    <div class="nes-field">
+    <!-- <div class="nes-field">
       <label for="name_field" style="color:#fff;">Cadence</label>
       <input type="text" id="name_field" class="nes-input is-dark">
     </div>
@@ -31,13 +107,19 @@
           <i class="nes-bcrikko"></i>
         </section>
       </section>
-    </section>
+    </section> -->
   </div>
   
 </template>
 
 <script>
+import Header from '../components/Header.vue'
 export default {
+
+  components: {
+    Header,
+  },
+
   mounted () {
     console.log(this.$near)
   }
