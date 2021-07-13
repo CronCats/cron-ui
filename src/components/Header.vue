427f06d94942b21290fe20a893228071b3be7b53
Trevor Clarke <TrevorJTClarke@users.noreply.github.com>
2021-07-13 00:06:51 -0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,7 +21,7 @@
             </svg>
             <span>Tasks</span>
           </a>
-          <a href="https://github.com/Cron-Near/croncat" target="_blank" class="flex whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-100">
+          <a href="https://docs.cron.cat/docs/agent-cli" target="_blank" class="flex whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-100">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
